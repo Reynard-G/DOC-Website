82efa1dcfc4065edb135415c1419946f29169b55
Reynard-G <reynard.ean.gunawan@gmail.com>
2023-06-25 11:05:24 -0500

    navbar interactions global

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,23 @@ import React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
+import ChartDropdownContext from 'pages/context';
+
 React.useLayoutEffect = React.useEffect;
 
 const MyApp = ({ Component, pageProps }) => {
+  const [chartDropdownOpen, setChartDropdownOpen] = React.useState(false);
+  const chartDropdownState = { chartDropdownOpen, setChartDropdownOpen };
+
   return (
     <NextUIProvider>
       <NextThemesProvider
         forcedTheme="dark"
         attribute="class"
       >
-        <Component {...pageProps} />
+        <ChartDropdownContext.Provider value={chartDropdownState}>
+          <Component {...pageProps} />
+        </ChartDropdownContext.Provider>
       </NextThemesProvider>
     </NextUIProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,12 @@ import React from 'react';
 import Layout from 'components/Layout';
 import HeroSection from 'components/HeroSection';
 
-import ChartDropdownContext from 'pages/context';
-
 export default function Home() {
-  const [chartDropdownOpen, setChartDropdownOpen] = React.useState(false);
-  const chartDropdownState = { chartDropdownOpen, setChartDropdownOpen };
-
   return (
     <>
-      <ChartDropdownContext.Provider value={chartDropdownState}>
-        <Layout title="DOC Analytics">
-          <HeroSection />
-        </Layout>
-      </ChartDropdownContext.Provider>
+      <Layout title="DOC Analytics">
+        <HeroSection />
+      </Layout>
     </>
   );
 };
