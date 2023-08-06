c41e90a06f45a509a3d9261c4cc1221ce7a19d0d
Reynard-G <reynard.ean.gunawan@gmail.com>
2023-08-05 22:34:55 -0500

    og metadata

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,22 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="og:title" content="The Department of Commerce" />
+          <meta name="og:description"
+            content="The Department of Commerce fosters economic growth, innovation, and job
+            creation through promoting trade, technology advancement, and data-driven policies." />
+          <meta name="og:image" content="https://imgs.milklegend.xyz/dept_commerce.png" />
+          <meta name="og:url" content="https://doc.milklegend.xyz" />
+          <meta name="og:type" content="website" />
+          <meta name="og:site_name" content="The Department of Commerce" />
+          <meta name="og:locale" content="en_US" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="The Department of Commerce" />
+          <meta name="twitter:description"
+            content="The Department of Commerce fosters economic growth, innovation, and job
+            creation through promoting trade, technology advancement, and data-driven policies." />
+          <meta name="twitter:image" content="https://imgs.milklegend.xyz/dept_commerce.png" />
+          <meta name="twitter:image:alt" content="The Department of Commerce" />
         </Head>
 
         <body>
