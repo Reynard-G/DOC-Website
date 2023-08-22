4dc035ad207997c948618ff78f298cbb8098fca5
Reynard-G <reynard.ean.gunawan@gmail.com>
2023-08-21 19:44:23 -0500

    content security policy

--- next.config.js
+++ next.config.js
@@ -13,3 +13,39 @@ module.exports = withBundleAnalyzer({
     isrMemoryCacheSize: 0,
   },
 });
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'script=default-src \'self\'; img-src \'self\'; font-src \'self\';',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+          },
+        ],
+      },
+    ];
+  }
+};
